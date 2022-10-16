93f2556560c4d3e5d0557436e95d320f03a0047e
samm459 <sam@contentcucumber.com>
2022-10-15 21:59:04 -0400

    Add token, description, and node fns to LiteralSyntax

--- src/syntax/literal.rs
+++ src/syntax/literal.rs
@@ -4,14 +4,28 @@ use crate::interpreter::{
     Interpreter,
 };
 
-use super::{Leaf, Token};
+use super::{Description, Leaf, Node, Token};
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct LiteralSyntax(pub Leaf);
 
 impl LiteralSyntax {
+    pub fn token(&self) -> &Token {
+        &self.node().0
+    }
+
+    pub fn description(&self) -> &Description {
+        &self.node().1
+    }
+
+    pub fn node(&self) -> &Node {
+        &self.0
+    }
+}
+
+impl LiteralSyntax {
     pub fn bind(&self) -> Type {
-        match self.0 .0 {
+        match self.token() {
             Token::String => Type::String,
             Token::Number => Type::Number,
             Token::Boolean => Type::Boolean,
@@ -21,7 +35,7 @@ impl LiteralSyntax {
     }
 
     pub fn eval(&self, interpreter: &mut Interpreter) -> Value {
-        match self.0 .0 {
+        match self.token() {
             Token::String => Value::String(inner_string(interpreter.source(self.0))),
             Token::Number => Value::Number(interpreter.source(self.0).parse::<usize>().unwrap()),
             Token::Boolean => Value::Boolean(interpreter.source(self.0).parse::<bool>().unwrap()),
